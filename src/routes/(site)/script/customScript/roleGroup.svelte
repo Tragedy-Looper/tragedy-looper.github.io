0e8f5f6d96dc7bcee8c3e452735fb806ea8ea146
Patrick Kranz <patrick-kranz@live.de>
2025-05-28 23:31:27 +0200

    related to #17

--- src/routes/(site)/script/customScript/roleGroup.svelte
+++ src/routes/(site)/script/customScript/roleGroup.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-	import type { CharacterName } from '../../../../model/characters';
-	import type { ICustomScriptRoleExclusiveSelectionGroup } from '../../../../model/customScript';
-	import RoleSelect from './roleSelect.svelte';
+  import type { CharacterName } from '../../../../model/characters';
+  import type { ICustomScriptRoleExclusiveSelectionGroup } from '../../../../model/customScript';
+  import Translation from '../../../../view/translation.svelte';
+  import RoleSelect from './roleSelect.svelte';
 
-	export let group: ICustomScriptRoleExclusiveSelectionGroup<CharacterName>;
-	$: numberOfRoles = group.selectedNumber;
-	$: selectors = group.selectors;
+  export let group: ICustomScriptRoleExclusiveSelectionGroup<CharacterName>;
+  $: numberOfRoles = group.selectedNumber;
+  $: selectors = group.selectors;
 </script>
 
 <h3>
-	{group.role}
+  {#if group.role === 'Person'}
+    <Translation translationKey={'Number of Characters not in Plot roles'} />
+  {:else}
+    {group.role}
+  {/if}
 </h3>
 
 {#if group.min !== group.max}
-	<label>
-		Number of {group.role}'s
-		<input type="number" bind:value={$numberOfRoles} min={group.min} max={group.max} />
-	</label>
+  <label>
+    {#if group.role === 'Person'}
+      <small>(<Translation translationKey={'This inculdes Characters like Mystery Boy'} />)</small>
+    {:else}
+      Number of {group.role}'s
+    {/if}
+    <input type="number" bind:value={$numberOfRoles} min={group.min} max={group.max} />
+  </label>
 {/if}
 
 {#each $selectors as role}
-	<RoleSelect {role} />
+  <RoleSelect {role} />
 {/each}
